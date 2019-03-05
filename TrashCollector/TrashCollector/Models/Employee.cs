22dbbf9c26446f30dc668020878ba49db3815cf4
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-05 12:46:42 -0600

    Added Employee class

--- TrashCollector/TrashCollector/Models/Employee.cs
+++ TrashCollector/TrashCollector/Models/Employee.cs
@@ -14,5 +14,7 @@ namespace TrashCollector.Models
         //public string Zip { get; set; }
         //public string Email { get; set; }
         //public string Password { get; set; }
+
+
     }
 }
