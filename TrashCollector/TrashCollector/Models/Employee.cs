565ff7cbd27d4cf83bb52654a3a5b93b6fffb91e
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-05 11:36:08 -0600

    Added model User

---
+++ TrashCollector/TrashCollector/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Zip { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+
+    }
+}
