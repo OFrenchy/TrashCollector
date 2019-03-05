64624711b73aefbaec3a7f0498a5fb214fd643c6
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-05 13:00:04 -0600

    Added Pickup class

---
+++ TrashCollector/TrashCollector/Models/Pickups.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Pickups
+    {
+        [Key]
+        public int ID { get; set; }
+
+        //[ForeignKey("ApplicationUser")]
+        //public string ApplicationUserId { get; set; }
+        //public ApplicationUser ApplicationUser { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+
+        public DateTime DateOfPickup { get; set; }
+        public string Zip { get; set; }
+        public int Price { get; set; }
+        public bool Paid { get; set; }
+
+    }
+}
