919977ac17525a7bb577599af56cdab96ec143af
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-06 14:42:32 -0600

    Recreated DB; couldn't add Controllers with Entity Framework; built out EmployeeController.cs

---
+++ TrashCollector/Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+
+namespace TrashCollector.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext db;
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            //return View(db.SuperHeroes.OrderBy(o => o.Name).ToList());
+            return View(db.Employees.OrderBy(o => o.ApplicationUser.Email).ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            //return View(db.SuperHeroes.Find(id));
+            return View(db.Employees.Find(id));
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                //db.SuperHeroes.Add(superHero);
+                //db.SaveChanges();
+                //return RedirectToAction("Index");
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                //return View(superHero);
+                return View(employee);
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(db.Employees.Find(id));
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Employee employee)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Employee thisEmployee = db.Employees.Find(id);
+                thisEmployee.Zip = employee.Zip;
+                // thisEmployee.ID
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(employee);
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            //return View();
+            Employee employee = db.Employees.Find(id);
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Employee employee)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                db.Employees.Remove(db.Employees.Find(id));
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(employee);
+            }
+        }
+    }
+}
